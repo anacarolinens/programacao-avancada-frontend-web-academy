e1f6316db1ed24127d73b734922cd6a70be1ca1c
Ana Caroline <caroline.ana.n.s@gmail.com>
2023-07-21 19:19:18 -0500

    Updates - 21/07/23

--- bootstrap-project/scripts.js
+++ bootstrap-project/scripts.js
@@ -1,14 +1,29 @@
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+//Progress bar
+let containerA = new ProgressBar.Circle(circleA, {
+  color: '#0b7b9e',
+  strokeWidth: 6,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: trues
+  },
+  from: { color: '#0b7b9e', width: 1 },
+  to: { color: '#0b7b9e', width: 4 },
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
 
-var bar = new ProgressBar.Circle(container, {
-    strokeWidth: 6,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#FFEA82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: null
-  });
-  
-  bar.animate(1.0);  // Number from 0.0 to 1.0
+    let value = Math.round(circle.value() * 60);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"roboto", sans-serif';
+bar.text.style.fontSize = '2rem';
+
+ContainerA.animate(1.0);
